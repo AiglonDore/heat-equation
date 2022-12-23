bd43e67a634d789944e0749cfd501e5b06f74aa8
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-12-23 18:40:54 +0100

    Updated doc

--- Documentation/html/search/all_a.js
+++ Documentation/html/search/all_a.js
@@ -4,6 +4,6 @@ var searchData=
   ['sdl_2ecpp_28',['sdl.cpp',['../sdl_8cpp.html',1,'']]],
   ['sdl_2eh_29',['sdl.h',['../sdl_8h.html',1,'']]],
   ['sdlbarrunwindow_30',['SdlBarRunWindow',['../class_sdl.html#ae96fdface86768f26f93296d4ed9511e',1,'Sdl']]],
-  ['solve_31',['solve',['../class_bar.html#abe693b09228fa57b5cd65293e912f06b',1,'Bar']]],
-  ['solvebar_32',['solveBar',['../class_bar.html#ad4de2e93998499a60aa387d8725544c2',1,'Bar::solveBar()'],['../computation_8h.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp'],['../computation_8cpp.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp']]]
+  ['solve_31',['solve',['../class_bar.html#aafb591cd4dd625f5e498074a0c3e020d',1,'Bar']]],
+  ['solvebar_32',['solveBar',['../computation_8h.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp'],['../computation_8cpp.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp']]]
 ];
--- Documentation/html/search/functions_6.js
+++ Documentation/html/search/functions_6.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['sdl_67',['Sdl',['../class_sdl.html#adc501e37d45c124f9382ded1a888f0a7',1,'Sdl']]],
   ['sdlbarrunwindow_68',['SdlBarRunWindow',['../class_sdl.html#ae96fdface86768f26f93296d4ed9511e',1,'Sdl']]],
-  ['solve_69',['solve',['../class_bar.html#abe693b09228fa57b5cd65293e912f06b',1,'Bar']]],
-  ['solvebar_70',['solveBar',['../class_bar.html#ad4de2e93998499a60aa387d8725544c2',1,'Bar::solveBar()'],['../computation_8h.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp'],['../computation_8cpp.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp']]]
+  ['solve_69',['solve',['../class_bar.html#aafb591cd4dd625f5e498074a0c3e020d',1,'Bar']]],
+  ['solvebar_70',['solveBar',['../computation_8h.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp'],['../computation_8cpp.html#adbb6a6e1d20fc59b4678e9d0d6218a7d',1,'solveBar(const Bar &amp;bar, const std::string &amp;filename, bool nogui):&#160;computation.cpp']]]
 ];
