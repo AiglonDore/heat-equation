430b17fad23d8bec5c35969248aed6903c6ccb08
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-12-24 17:48:27 +0100

    Wrote installation script

--- install.sh
+++ install.sh
@@ -4,6 +4,17 @@ sudo apt update
 
 sudo apt install g++ libsdl2-dev make
 
+if [ -z -f Makefile ]; then
+    echo "Makefile not found"
+    exit 1
+fi
+if [ -z -d bin ]; then
+    mkdir bin
+fi
+if [ -z -d obj ]; then
+    mkdir obj
+fi
+
 # Build the appliation
 sudo make install
 
